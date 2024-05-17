4cb4784aa1aaf22cd35ea1e10bd3577e76bf0c36
RakibHossen678 <rh491464@gmail.com>
2024-05-17 16:47:50 +0600

    find review data from database

--- index.js
+++ index.js
@@ -28,9 +28,14 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     
     const menuCollection=client.db('BISTROBOSSDB').collection('menu')
+    const reviewsCollection=client.db('BISTROBOSSDB').collection('reviews')
 
     app.get('/menu',async(req,res)=>{
         const result=await menuCollection.find().toArray()
+        res.send(result)
+    })
+    app.get('/review',async(req,res)=>{
+        const result=await reviewsCollection.find().toArray()
         res.send(result)
     })
 
