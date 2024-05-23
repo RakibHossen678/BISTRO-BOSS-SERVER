fe467b8eec8c89ea882ca566a4904228bcad0ecc
RakibHossen678 <rh491464@gmail.com>
2024-05-23 19:37:39 +0600

    verify user

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
+const jwt = require("jsonwebtoken");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
@@ -28,19 +29,99 @@ async function run() {
     const reviewsCollection = client.db("BISTROBOSSDB").collection("reviews");
     const cartsCollection = client.db("BISTROBOSSDB").collection("carts");
 
+    //jwt related api
 
-    app.post('/users',async(req,res)=>{
-      const user=req.body
-      //insert email if user does not exists:
-      const query={email:user.email}
-      const existingUser=await userCollection.findOne(query)
-      if(existingUser){
-        return res.send({message:'user already exists' , insertedId:null})
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      res.send({ token });
+    });
+
+    const verifyToken = (req, res, next) => {
+      console.log("inside", req.headers.authorization);
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: "forbidden access" });
       }
-      const result=await userCollection.insertOne(user)
-      res.send(result)
-    })
+      const token = req.headers.authorization.split(" ")[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.send(401).send({ message: "forbidden access" });
+        }
+        req.decoded = decoded;
+        next();
+      });
+    };
 
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      const isAdmin = user?.role === "admin";
+      if (!isAdmin) {
+        return res.status(403).send({ message: "forbidden access" });
+      }
+      next();
+    };
+
+    //user related api
+
+    app.get("/users", verifyToken,verifyAdmin, async (req, res) => {
+      // console.log()
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      //insert email if user does not exists:
+      const query = { email: user.email };
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "user already exists", insertedId: null });
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+      const result = await userCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    });
+
+    app.get("/users/admin/:email", verifyToken,verifyAdmin, async (req, res) => {
+      const email = req.params.email;
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: "unauthorized" });
+      }
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      console.log(user);
+      let admin = false;
+      if (user) {
+        admin = user?.role === "admin";
+        console.log(admin);
+      }
+      console.log(admin);
+      res.send({ admin });
+    });
+
+    //menu related api
     app.get("/menu", async (req, res) => {
       const result = await menuCollection.find().toArray();
       res.send(result);
@@ -65,8 +146,8 @@ async function run() {
     app.delete("/carts/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
-      const result=await cartsCollection.deleteOne(query)
-      res.send(result)
+      const result = await cartsCollection.deleteOne(query);
+      res.send(result);
     });
 
     // Send a ping to confirm a successful connection
