a57ca3a1098a34fad54e852ceac69dcefd440211
RakibHossen678 <rh491464@gmail.com>
2024-05-28 13:45:06 +0600

    filter data using pipeline

--- index.js
+++ index.js
@@ -109,7 +109,7 @@ async function run() {
     app.get(
       "/users/admin/:email",
       verifyToken,
-      
+
       async (req, res) => {
         const email = req.params.email;
         if (email !== req.decoded.email) {
@@ -215,19 +215,93 @@ async function run() {
           $in: payment.cartIds.map((id) => new ObjectId(id)),
         },
       };
-     
+
       const deleteResult = await cartsCollection.deleteMany(query);
       res.send({ deleteResult, paymentResult });
     });
 
-    app.get('/payments/:email',verifyToken,async(req,res)=>{
-      const query={email:req.params.email}
-      if(req.params.email !==req.decoded.email){
-        return res.status(403).send({message:'forbidden access'})
+    app.get("/payments/:email", verifyToken, async (req, res) => {
+      const query = { email: req.params.email };
+      if (req.params.email !== req.decoded.email) {
+        return res.status(403).send({ message: "forbidden access" });
       }
-      const result=await paymentCollection.find(query).toArray()
-      res.send(result)
-    })
+      const result = await paymentCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    //stats of analytics
+    app.get("/admin-stats", verifyToken, verifyAdmin, async (req, res) => {
+      const users = await userCollection.estimatedDocumentCount();
+      const menuItems = await menuCollection.estimatedDocumentCount();
+      const orders = await paymentCollection.estimatedDocumentCount();
+      //this is not the best way
+      // const payments=await paymentCollection.find().toArray()
+      // const revenue=payments.reduce((total,payment)=>total+payment.price,0)
+      const result = await paymentCollection
+        .aggregate([
+          {
+            $group: {
+              _id: null,
+              totalRevenue: {
+                $sum: "$price",
+              },
+            },
+          },
+        ])
+        .toArray();
+      const revenue = result.length > 0 ? result[0].totalRevenue : 0;
+      res.send({
+        users,
+        menuItems,
+        orders,
+        revenue,
+      });
+    });
+
+    //order stats using aggregate pipeline
+    app.get("/order-stats",verifyToken,verifyAdmin, async (req, res) => {
+      const result = await paymentCollection
+        .aggregate([
+          {
+            $unwind: "$menuIds",
+          },
+          {
+            $addFields: {
+              menuIdObject: { $toObjectId: "$menuIds" },
+            },
+          },
+          {
+            $lookup: {
+              from: "menu",
+              localField: "menuIdObject",
+              foreignField: "_id",
+              as: "menuItems",
+            },
+          },
+          {
+            $unwind: "$menuItems",
+          },
+          {
+            $group: {
+              _id: "$menuItems.category",
+              quantity: {
+                $sum: 1,
+              },
+              revenue:{$sum:'$menuItems.price'}
+            },
+          },
+          {
+            $project:{
+              _id:0,
+              category:'$_id',
+              quantity:'$quantity',
+              revenue:'$revenue'
+            }
+          },
+        ])
+        .toArray();
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     console.log(
